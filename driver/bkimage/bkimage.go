8b8725d1fdca2e0bc4eda8317c16db0c64ac9225
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-12-16 17:35:38 +0900

    set DefaultRootlessImage to "moby/buildkit:buildx-stable-1-rootless"
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- driver/bkimage/bkimage.go
+++ driver/bkimage/bkimage.go
@@ -2,5 +2,5 @@ package bkimage
 
 const (
 	DefaultImage         = "moby/buildkit:buildx-stable-1" // TODO: make this verified
-	DefaultRootlessImage = "moby/buildkit:v0.6.2-rootless"
+	DefaultRootlessImage = DefaultImage + "-rootless"
 )
