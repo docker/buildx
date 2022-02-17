7677052cb70b25d8a389896077212e867ea9a50b
CrazyMax <crazy-max@users.noreply.github.com>
2022-02-17 21:01:52 +0100

    build: send buildinfo dependencies
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

---
+++ vendor/github.com/moby/buildkit/exporter/containerimage/exptypes/types.go
@@ -0,0 +1,28 @@
+package exptypes
+
+import (
+	digest "github.com/opencontainers/go-digest"
+	ocispecs "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+const (
+	ExporterConfigDigestKey      = "config.digest"
+	ExporterImageDigestKey       = "containerimage.digest"
+	ExporterImageConfigKey       = "containerimage.config"
+	ExporterImageConfigDigestKey = "containerimage.config.digest"
+	ExporterImageDescriptorKey   = "containerimage.descriptor"
+	ExporterInlineCache          = "containerimage.inlinecache"
+	ExporterBuildInfo            = "containerimage.buildinfo"
+	ExporterPlatformsKey         = "refs.platforms"
+)
+
+const EmptyGZLayer = digest.Digest("sha256:4f4fb700ef54461cfa02571ae0db9a0dc1e0cdb5577484a6d75e68dc38e8acc1")
+
+type Platforms struct {
+	Platforms []Platform
+}
+
+type Platform struct {
+	ID       string
+	Platform ocispecs.Platform
+}
