b65f49622e3769da480876bcc70349d1f9962a50
Justin Chadwell <me@jedevc.com>
2023-03-08 16:25:00 +0000

    version: add "v" prefix to version for tagging convention consistency
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ var (
 	Package = "github.com/docker/buildx"
 
 	// Version holds the complete version number. Filled in at linking time.
-	Version = "0.0.0+unknown"
+	Version = "v0.0.0+unknown"
 
 	// Revision is filled with the VCS (e.g. git) revision being used to build
 	// the program at linking time.
