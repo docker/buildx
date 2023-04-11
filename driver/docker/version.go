1b816ff8387159143deeebdbfc2665274ff29997
CrazyMax <crazy-max@users.noreply.github.com>
2023-04-11 11:10:01 +0200

    driver: update BuildKit version constraint for docker driver
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- driver/docker/version.go
+++ driver/docker/version.go
@@ -153,7 +153,7 @@ var mobyBuildkitVersions = []mobyBuildkitVersion{
 		BuildkitVersion:       "v0.10.6+4f0ee09",
 	},
 	{
-		MobyVersionConstraint: "23.0.2",
+		MobyVersionConstraint: ">= 23.0.2-0, < 23.0.4-0",
 		BuildkitVersion:       "v0.10.6+70f2ad5",
 	},
 	{
