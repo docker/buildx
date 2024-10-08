52cddf9ee503adbfb5bee98fcaf29109761f2a66
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-10-08 13:38:13 +0200

    build: fix build details link in experimental mode
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- controller/errdefs/build.go
+++ controller/errdefs/build.go
@@ -1,7 +1,10 @@
 package errdefs
 
 import (
+	"io"
+
 	"github.com/containerd/typeurl/v2"
+	"github.com/docker/buildx/util/desktop"
 	"github.com/moby/buildkit/util/grpcerrors"
 )
 
@@ -22,11 +25,22 @@ func (e *BuildError) ToProto() grpcerrors.TypedErrorProto {
 	return e.Build
 }
 
-func WrapBuild(err error, ref string) error {
+func (e *BuildError) PrintBuildDetails(w io.Writer) error {
+	if e.BuildRef == "" {
+		return nil
+	}
+	ebr := &desktop.ErrorWithBuildRef{
+		Ref: e.BuildRef,
+		Err: e.error,
+	}
+	return ebr.Print(w)
+}
+
+func WrapBuild(err error, ref string, buildRef string) error {
 	if err == nil {
 		return nil
 	}
-	return &BuildError{Build: &Build{Ref: ref}, error: err}
+	return &BuildError{Build: &Build{Ref: ref, BuildRef: buildRef}, error: err}
 }
 
 func (b *Build) WrapError(err error) error {
