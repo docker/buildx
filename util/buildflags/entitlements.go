6272ae1afa0c5bb13474e9dc6089db7cef3104b5
Tonis Tiigi <tonistiigi@gmail.com>
2025-02-20 15:41:13 -0800

    remove accidental debug
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- util/buildflags/entitlements.go
+++ util/buildflags/entitlements.go
@@ -1,14 +1,11 @@
 package buildflags
 
 import (
-	"log"
-
 	"github.com/moby/buildkit/util/entitlements"
 )
 
 func ParseEntitlements(in []string) ([]string, error) {
 	out := make([]string, 0, len(in))
-	log.Printf("in: %#v", in)
 	for _, v := range in {
 		if v == "" {
 			continue
@@ -19,6 +16,5 @@ func ParseEntitlements(in []string) ([]string, error) {
 		}
 		out = append(out, v)
 	}
-	log.Printf("Parsed entitlements: %v", out)
 	return out, nil
 }
