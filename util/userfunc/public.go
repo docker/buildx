8d173d192c7f9ac2e02cfeda310fb1b79a40a553
Tonis Tiigi <tonistiigi@gmail.com>
2025-04-14 10:56:20 -0700

    bake: fix variadic_params inconsistency for user functions
    There was inconsistency between variables used for function
    definitions in HCL and JSON format. Updated JSON to match HCL,
    fixed documentation and removed the unused code from userfunc
    pkg (based on HCL upstream) to avoid confusion.
    
    Theoretically we could add some temporary backwards compatibility
    for the JSON format but I think it is unlikely that someone uses
    JSON format for this and also defined variadic parameters.
    
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- util/userfunc/public.go
+++ util/userfunc/public.go
@@ -20,28 +20,6 @@ import (
 // structure a function declaration is found, etc.
 type ContextFunc func() *hcl.EvalContext
 
-// DecodeUserFunctions looks for blocks of the given type in the given body
-// and, for each one found, interprets it as a custom function definition.
-//
-// On success, the result is a mapping of function names to implementations,
-// along with a new body that represents the remaining content of the given
-// body which can be used for further processing.
-//
-// The result expression of each function is parsed during decoding but not
-// evaluated until the function is called.
-//
-// If the given ContextFunc is non-nil, it will be called to obtain the
-// context in which the function result expressions will be evaluated. If nil,
-// or if it returns nil, the result expression will have access only to
-// variables named after the declared parameters. A non-nil context turns
-// the returned functions into closures, bound to the given context.
-//
-// If the returned diagnostics set has errors then the function map and
-// remain body may be nil or incomplete.
-func DecodeUserFunctions(body hcl.Body, blockType string, context ContextFunc) (funcs map[string]function.Function, remain hcl.Body, diags hcl.Diagnostics) {
-	return decodeUserFunctions(body, blockType, context)
-}
-
 // NewFunction creates a new function instance from preparsed HCL expressions.
 func NewFunction(paramsExpr, varParamExpr, resultExpr hcl.Expression, getBaseCtx func() *hcl.EvalContext) (function.Function, hcl.Diagnostics) {
 	var params []string
@@ -69,7 +47,7 @@ func NewFunction(paramsExpr, varParamExpr, resultExpr hcl.Expression, getBaseCtx
 		if varParam == "" {
 			return function.Function{}, hcl.Diagnostics{{
 				Severity: hcl.DiagError,
-				Summary:  "Invalid variadic_param",
+				Summary:  "Invalid variadic_params",
 				Detail:   "The variadic parameter name must be an identifier.",
 				Subject:  varParamExpr.Range().Ptr(),
 			}}
