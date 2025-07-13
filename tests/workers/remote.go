226041ea046602051ddb9f332dcb3a81c7407a54
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-07-13 11:15:22 +0200

    tests: run remote worker on tcp and container endpoints
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- tests/workers/remote.go
+++ tests/workers/remote.go
@@ -2,63 +2,121 @@ package workers
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"os"
 	"os/exec"
+	"sync"
 
 	"github.com/moby/buildkit/identity"
 	"github.com/moby/buildkit/util/testutil/integration"
-	bkworkers "github.com/moby/buildkit/util/testutil/workers"
 	"github.com/pkg/errors"
 )
 
+var protos = []string{
+	"docker-container",
+	"tcp",
+}
+
 func InitRemoteWorker() {
-	integration.Register(&remoteWorker{
-		id: "remote",
-	})
+	for _, p := range protos {
+		integration.Register(&remoteWorker{
+			id:    "remote+" + p,
+			proto: p,
+		})
+	}
 }
 
 type remoteWorker struct {
-	id          string
+	id    string
+	proto string
+
 	unsupported []string
+
+	docker      integration.Backend
+	dockerClose func() error
+	dockerErr   error
+	dockerOnce  sync.Once
 }
 
-func (w remoteWorker) Name() string {
+func (w *remoteWorker) Name() string {
 	return w.id
 }
 
-func (w remoteWorker) Rootless() bool {
+func (w *remoteWorker) Rootless() bool {
 	return false
 }
 
-func (w remoteWorker) NetNSDetached() bool {
+func (w *remoteWorker) NetNSDetached() bool {
 	return false
 }
 
-func (w remoteWorker) New(ctx context.Context, cfg *integration.BackendConfig) (b integration.Backend, cl func() error, err error) {
-	oci := bkworkers.OCI{ID: w.id}
-	bk, bkclose, err := oci.New(ctx, cfg)
-	if err != nil {
-		return bk, cl, err
+func (w *remoteWorker) New(ctx context.Context, cfg *integration.BackendConfig) (b integration.Backend, cl func() error, err error) {
+	w.dockerOnce.Do(func() {
+		w.docker, w.dockerClose, w.dockerErr = dockerWorker{id: w.id}.New(ctx, cfg)
+	})
+	if w.dockerErr != nil {
+		return w.docker, w.dockerClose, w.dockerErr
 	}
 
+	bkCtnName := "buildkit-integration-" + identity.NewID()
 	name := "integration-remote-" + identity.NewID()
+	envs := append(
+		os.Environ(),
+		"BUILDX_CONFIG=/tmp/buildx-"+name,
+		"DOCKER_CONTEXT="+w.docker.DockerAddress(),
+	)
+
+	// random host port for buildkit container
+	l, _ := net.Listen("tcp", ":0") //nolint:gosec
+	_ = l.Close()
+	bkPort := l.Addr().(*net.TCPAddr).Port
+
+	// create buildkit container
+	bkCtnCmd := exec.Command("docker", "run",
+		"-d", "--rm",
+		"--privileged",
+		"-p", fmt.Sprintf("%d:1234", bkPort),
+		"--name="+bkCtnName,
+		"moby/buildkit:buildx-stable-1",
+		"--addr=tcp://0.0.0.0:1234",
+	)
+	bkCtnCmd.Env = envs
+	if out, err := bkCtnCmd.CombinedOutput(); err != nil {
+		return nil, nil, errors.Wrapf(err, "failed to create buildkit container %s: %s", bkCtnName, string(out))
+	}
+
+	// create builder
+	var endpoint string
+	switch w.proto {
+	case "docker-container":
+		endpoint = fmt.Sprintf("docker-container://%s", bkCtnName)
+	case "tcp":
+		endpoint = fmt.Sprintf("tcp://localhost:%d", bkPort)
+	default:
+		return nil, nil, errors.Errorf("unsupported protocol %s", w.proto)
+	}
 	cmd := exec.Command("buildx", "create",
 		"--bootstrap",
 		"--name="+name,
 		"--driver=remote",
-		bk.Address(),
+		endpoint,
 	)
-	cmd.Env = append(os.Environ(), "BUILDX_CONFIG=/tmp/buildx-"+name)
-	if err := cmd.Run(); err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to create buildx instance %s", name)
+	cmd.Env = envs
+	if out, err := cmd.CombinedOutput(); err != nil {
+		return nil, nil, errors.Wrapf(err, "failed to create buildx instance %s: %s", name, string(out))
 	}
 
 	cl = func() error {
-		err := bkclose()
-		cmd := exec.Command("buildx", "rm", "-f", name)
-		cmd.Env = append(os.Environ(), "BUILDX_CONFIG=/tmp/buildx-"+name)
-		if err1 := cmd.Run(); err == nil {
-			err = err1
+		cmd := exec.Command("docker", "container", "rm", "-f", name)
+		cmd.Env = envs
+		if err1 := cmd.Run(); err1 != nil {
+			err = errors.Wrapf(err1, "failed to remove buildkit container %s", bkCtnName)
+		}
+		cmd = exec.Command("buildx", "rm", "-f", name)
+		cmd.Env = envs
+		if err1 := cmd.Run(); err1 != nil {
+			err = errors.Wrapf(err1, "failed to remove buildx instance %s", name)
 		}
 		return err
 	}
@@ -69,6 +127,6 @@ func (w remoteWorker) New(ctx context.Context, cfg *integration.BackendConfig) (
 	}, cl, nil
 }
 
-func (w remoteWorker) Close() error {
+func (w *remoteWorker) Close() error {
 	return nil
 }
