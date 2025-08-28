95a38bbfc41157ed510723340675fe3c7ef4eb95
Sebastiaan van Stijn <github@gone.nl>
2025-08-28 17:19:37 +0200

    vendor: moby/moby/api v1.52.0-beta.1, moby/moby/client v0.1.0-beta.0
    full diffs:
    
    - https://github.com/moby/moby/compare/client/v0.1.0-alpha.0...client/v0.1.0-beta.0
    - https://github.com/moby/moby/compare/api/v1.52.0-alpha.1...api/v1.52.0-beta.1
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/moby/moby/api/types/container/container.go
+++ vendor/github.com/moby/moby/api/types/container/container.go
@@ -27,13 +27,6 @@ type PathStat struct {
 	LinkTarget string      `json:"linkTarget"`
 }
 
-// CopyToContainerOptions holds information
-// about files to copy into a container
-type CopyToContainerOptions struct {
-	AllowOverwriteDirWithFile bool
-	CopyUIDGID                bool
-}
-
 // MountPoint represents a mount point configuration inside the container.
 // This is used for reporting the mountpoints in use by a container.
 type MountPoint struct {
@@ -104,7 +97,7 @@ type Summary struct {
 	ImageManifestDescriptor *ocispec.Descriptor `json:"ImageManifestDescriptor,omitempty"`
 	Command                 string
 	Created                 int64
-	Ports                   []Port
+	Ports                   []PortSummary
 	SizeRw                  int64 `json:",omitempty"`
 	SizeRootFs              int64 `json:",omitempty"`
 	Labels                  map[string]string
@@ -119,16 +112,9 @@ type Summary struct {
 	Mounts          []MountPoint
 }
 
-// ContainerJSONBase contains response of Engine API GET "/containers/{name:.*}/json"
-// for API version 1.18 and older.
-//
-// TODO(thaJeztah): combine ContainerJSONBase and InspectResponse into a single struct.
-// The split between ContainerJSONBase (ContainerJSONBase) and InspectResponse (InspectResponse)
-// was done in commit 6deaa58ba5f051039643cedceee97c8695e2af74 (https://github.com/moby/moby/pull/13675).
-// ContainerJSONBase contained all fields for API < 1.19, and InspectResponse
-// held fields that were added in API 1.19 and up. Given that the minimum
-// supported API version is now 1.24, we no longer use the separate type.
-type ContainerJSONBase struct {
+// InspectResponse is the response for the GET "/containers/{name:.*}/json"
+// endpoint.
+type InspectResponse struct {
 	ID              string `json:"Id"`
 	Created         string
 	Path            string
@@ -151,12 +137,6 @@ type ContainerJSONBase struct {
 	GraphDriver     storage.DriverData
 	SizeRw          *int64 `json:",omitempty"`
 	SizeRootFs      *int64 `json:",omitempty"`
-}
-
-// InspectResponse is the response for the GET "/containers/{name:.*}/json"
-// endpoint.
-type InspectResponse struct {
-	*ContainerJSONBase
 	Mounts          []MountPoint
 	Config          *Config
 	NetworkSettings *NetworkSettings
--- vendor/github.com/moby/moby/client/checkpoint_delete.go
+++ vendor/github.com/moby/moby/client/checkpoint_delete.go
@@ -3,12 +3,16 @@ package client
 import (
 	"context"
 	"net/url"
-
-	"github.com/moby/moby/api/types/checkpoint"
 )
 
+// CheckpointDeleteOptions holds parameters to delete a checkpoint from a container.
+type CheckpointDeleteOptions struct {
+	CheckpointID  string
+	CheckpointDir string
+}
+
 // CheckpointDelete deletes the checkpoint with the given name from the given container.
-func (cli *Client) CheckpointDelete(ctx context.Context, containerID string, options checkpoint.DeleteOptions) error {
+func (cli *Client) CheckpointDelete(ctx context.Context, containerID string, options CheckpointDeleteOptions) error {
 	containerID, err := trimID("container", containerID)
 	if err != nil {
 		return err
@@ -20,6 +24,6 @@ func (cli *Client) CheckpointDelete(ctx context.Context, containerID string, opt
 	}
 
 	resp, err := cli.delete(ctx, "/containers/"+containerID+"/checkpoints/"+options.CheckpointID, query, nil)
-	ensureReaderClosed(resp)
+	defer ensureReaderClosed(resp)
 	return err
 }
