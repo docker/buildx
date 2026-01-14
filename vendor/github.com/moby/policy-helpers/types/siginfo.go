d54f398c5d1fe76dac127a877e4bbd8a8fa57c80
Tonis Tiigi <tonistiigi@gmail.com>
2026-01-14 13:36:15 -0800

    vendor: update buildkit to v0.27.0-rc1
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- vendor/github.com/moby/policy-helpers/types/siginfo.go
+++ vendor/github.com/moby/policy-helpers/types/siginfo.go
@@ -6,6 +6,51 @@ import (
 	"github.com/sigstore/sigstore-go/pkg/fulcio/certificate"
 )
 
+type Kind int
+
+const (
+	KindDockerGithubBuilder  Kind = 1
+	KindDockerHardenedImage  Kind = 2
+	KindSelfSignedGithubRepo Kind = 3
+	KindSelfSigned           Kind = 4
+	KindUntrusted            Kind = 1000
+)
+
+func (k Kind) String() string {
+	switch k {
+	case KindDockerGithubBuilder:
+		return "Docker GitHub Builder"
+	case KindDockerHardenedImage:
+		return "Docker Hardened Image"
+	case KindSelfSignedGithubRepo:
+		return "GitHub Self-Signed"
+	case KindSelfSigned:
+		return "Self-Signed"
+	case KindUntrusted:
+		return "Untrusted"
+	default:
+		return "Invalid"
+	}
+}
+
+type SignatureType int
+
+const (
+	SignatureBundleV03       SignatureType = 1
+	SignatureSimpleSigningV1 SignatureType = 2
+)
+
+func (st SignatureType) String() string {
+	switch st {
+	case SignatureBundleV03:
+		return "Sigstore Bundle"
+	case SignatureSimpleSigningV1:
+		return "SimpleSigning v1"
+	default:
+		return "Unknown"
+	}
+}
+
 type TimestampVerificationResult struct {
 	Type      string    `json:"type"`
 	URI       string    `json:"uri"`
@@ -18,6 +63,8 @@ type TrustRootStatus struct {
 }
 
 type SignatureInfo struct {
+	Kind            Kind                          `json:"kind"`
+	SignatureType   SignatureType                 `json:"signatureType"`
 	Signer          *certificate.Summary          `json:"signer,omitempty"`
 	Timestamps      []TimestampVerificationResult `json:"timestamps,omitempty"`
 	DockerReference string                        `json:"dockerReference,omitempty"`
