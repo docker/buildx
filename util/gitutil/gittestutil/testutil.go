720f91fdd4d1ee045d7a4290d1326c936acdc47c
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2026-03-12 15:41:38 +0100

    gitutil: migrate to BuildKit GitCLI API
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- util/gitutil/gittestutil/testutil.go
+++ util/gitutil/gittestutil/testutil.go
@@ -1,15 +1,17 @@
 package gittestutil
 
 import (
+	"context"
 	"os"
 	"strings"
 	"testing"
 
 	"github.com/docker/buildx/util/gitutil"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
 
-func GitInit(c *gitutil.Git, tb testing.TB) {
+func GitInit(c *gitutil.GitCLI, tb testing.TB) {
 	tb.Helper()
 	out, err := fakeGit(c, "init")
 	require.NoError(tb, err)
@@ -19,48 +21,48 @@ func GitInit(c *gitutil.Git, tb testing.TB) {
 	_, _ = fakeGit(c, "branch", "-D", "master")
 }
 
-func GitCommit(c *gitutil.Git, tb testing.TB, msg string) {
+func GitCommit(c *gitutil.GitCLI, tb testing.TB, msg string) {
 	tb.Helper()
 	out, err := fakeGit(c, "commit", "--allow-empty", "-m", msg)
 	require.NoError(tb, err)
 	require.Contains(tb, out, "main", msg)
 }
 
-func GitTag(c *gitutil.Git, tb testing.TB, tag string) {
+func GitTag(c *gitutil.GitCLI, tb testing.TB, tag string) {
 	tb.Helper()
 	out, err := fakeGit(c, "tag", tag)
 	require.NoError(tb, err)
 	require.Empty(tb, out)
 }
 
-func GitTagAnnotated(c *gitutil.Git, tb testing.TB, tag, message string) {
+func GitTagAnnotated(c *gitutil.GitCLI, tb testing.TB, tag, message string) {
 	tb.Helper()
 	out, err := fakeGit(c, "tag", "-a", tag, "-m", message)
 	require.NoError(tb, err)
 	require.Empty(tb, out)
 }
 
-func GitCheckoutBranch(c *gitutil.Git, tb testing.TB, name string) {
+func GitCheckoutBranch(c *gitutil.GitCLI, tb testing.TB, name string) {
 	tb.Helper()
 	out, err := fakeGit(c, "checkout", "-b", name)
 	require.NoError(tb, err)
 	require.Empty(tb, out)
 }
 
-func GitAdd(c *gitutil.Git, tb testing.TB, files ...string) {
+func GitAdd(c *gitutil.GitCLI, tb testing.TB, files ...string) {
 	tb.Helper()
 	args := append([]string{"add"}, files...)
 	_, err := fakeGit(c, args...)
 	require.NoError(tb, err)
 }
 
-func GitSetRemote(c *gitutil.Git, tb testing.TB, name string, url string) {
+func GitSetRemote(c *gitutil.GitCLI, tb testing.TB, name string, url string) {
 	tb.Helper()
 	_, err := fakeGit(c, "remote", "add", name, url)
 	require.NoError(tb, err)
 }
 
-func GitSetMainUpstream(c *gitutil.Git, tb testing.TB, remote, target string) {
+func GitSetMainUpstream(c *gitutil.GitCLI, tb testing.TB, remote, target string) {
 	tb.Helper()
 	_, err := fakeGit(c, "fetch", "--depth", "1", remote, target)
 	require.NoError(tb, err)
@@ -81,7 +83,7 @@ func Mktmp(tb testing.TB) string {
 	return folder
 }
 
-func fakeGit(c *gitutil.Git, args ...string) (string, error) {
+func fakeGit(c *gitutil.GitCLI, args ...string) (string, error) {
 	allArgs := []string{
 		"-c", "user.name=buildx",
 		"-c", "user.email=buildx@docker.com",
@@ -90,7 +92,15 @@ func fakeGit(c *gitutil.Git, args ...string) (string, error) {
 		"-c", "log.showSignature=false",
 	}
 	allArgs = append(allArgs, args...)
-	return c.Run(allArgs...)
+	return clean(c.Run(context.TODO(), allArgs...))
+}
+
+func clean(dt []byte, err error) (string, error) {
+	out := strings.ReplaceAll(strings.Split(string(dt), "\n")[0], "'", "")
+	if err != nil {
+		err = errors.New(strings.TrimSuffix(err.Error(), "\n"))
+	}
+	return out, err
 }
 
 func IsAmbiguousArgument(err error) bool {
